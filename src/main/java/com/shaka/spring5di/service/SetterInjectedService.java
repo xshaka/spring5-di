b390dacf62cece69bdbc01a9bdb3593bb5fb5b82
Ray W. Robinson Valiente <shaka.shadows@gmail.com>
2018-04-24 21:34:44 -0700

    Sample usage for Qualifiers annotation.

--- src/main/java/com/shaka/spring5di/service/SetterInjectedService.java
+++ src/main/java/com/shaka/spring5di/service/SetterInjectedService.java
@@ -2,6 +2,7 @@ package com.shaka.spring5di.service;
 
 import com.shaka.spring5di.component.PeopleGreeter;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -10,7 +11,8 @@ public class SetterInjectedService {
     private PeopleGreeter greeter;
 
     @Autowired
-    public void setGreeter(PeopleGreeter greeter) {
+    public void setGreeter(
+        @Qualifier("setter") PeopleGreeter greeter) {
         this.greeter = greeter;
     }
 
